7946d144a9f0a6f20dc1b300430e7cf63e473d98
shturkhal <107053994+shturkhal@users.noreply.github.com>
2024-01-08 20:27:22 +0200

    Initial Commit

---
+++ NoA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoA
+//
+//  Created by Іван Штурхаль on 08.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoA/NoAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoAApp.swift
+//  NoA
+//
+//  Created by Іван Штурхаль on 08.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NoAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
